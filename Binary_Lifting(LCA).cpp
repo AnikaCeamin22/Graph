a2f2f9cfee7655885a1ed7f8989e4c9826e58f8d
AnikaCeamin22 <155265333+AnikaCeamin22@users.noreply.github.com>
2025-05-26 17:01:08 +0600

    Add files via upload

---
+++ Binary_Lifting(LCA).cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//NlogN
+ int N=3e5+9,LOG=19;
+vector<vector<int>>par(N,vector<int>(LOG,0)),gh(N,vector<int>(LOG,0));
+vector<int>level(N,0);
+
+void BinaryLifting(int node, int p=0){
+    par[node][0]=p;   // set 1st parent
+    level[node]=level[p]+1;   // compute hight
+
+    for(int i=1; i<LOG; i++) par[node][i]=par[ par[node][i-1] ][i-1];
+
+    for(auto child : gh[node])
+        if(child!=p)
+            BinaryLifting(child,node);
+
+    return;
+}
+
+int LCA(int u, int v){
+    if(level[u]<level[v]) swap(u,v);
+
+    int d=level[u]-level[v];
+    while(d>0){
+        int i=log2(d);
+        u=par[u][i];
+        d=d-(1<<i);  // d=d-(2^i)
+    }
+
+    if(u==v) return u;
+
+    for(int k = LOG-1; k>=0; k--){
+        if(par[u][k] != par[v][k]){
+            u=par[u][k];
+            v=par[v][k];
+        }
+    }
+    return par[u][0];
+}
+
+int Kth(int node, int k){
+    if(level[node]<k) return -1;
+
+    for(int i=0; i<LOG; i++)
+        if(k & (1<<i)) node=par[node][i];
+
+    if(node==0) return -1;
+    return node;
+}
+
+int dist(int u, int v){
+    int lca=LCA(u,v);
+    return level[u]+level[v]-2*level[lca];
+}
+
+int32_t main() {
+  int n,q;
+  cin >> n >>q;
+
+  N=n+9;
+  LOG=log2(N);
+  par.resize(n+9);
+  par[0].resize(LOG);
+  level.resize(N);
+  gh.resize(N);
+  for(int i=0; i<N; i++) gh[i].resize(N),par[i].resize(LOG);
+
+  for (int i = 1; i < n; i++) {
+    int u,v; cin >> u>>v ;
+    gh[u].push_back(v);
+    gh[v].push_back(u);
+  }
+  BinaryLifting(1);
+  while (q--) {
+    int u,v;
+    cin>>u>>v;
+    cout << dist(u,v) << '\n';
+  }
+  return 0;
+}
